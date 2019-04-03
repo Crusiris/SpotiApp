717281b1b0fe69926aaf1616e2167565e0c72f9a
crusiris <cruzsirys@gmail.com>
2019-04-04 00:28:20 +0200

    centralizacion de las peticiones hacia spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,29 +9,27 @@ export class SpotifyService {
   constructor(private http: HttpClient) {
     console.log("Spotify Servicio Listo");
   }
-  getNewRelease() {
+
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
+
     const headers = new HttpHeaders({
       Authorization:
-        "Bearer BQBWwLrSGv9ELtrrx4Lj1L86vjKu63Rt9cXByzeV9gFneKi4H-SXSvhGo_dIbhqReoq2JuC62mjedzoVC_I"
+        "Bearer BQDADvFuBFpLfLAcsHldTR-eN7RXkSWSqy00vYgcWnRoRcGZDXCHWj8NfH75VsyiUwuy61T9BiNu7VPZZ68"
     });
 
-    return this.http
-      .get("https://api.spotify.com/v1/browse/new-releases?limit=20", {
-        headers
-      })
-      .pipe(map(data => data["albums"].items));
+    return this.http.get(url, { headers });
   }
-  getArtista(termino: string) {
-    const headers = new HttpHeaders({
-      Authorization:
-        "Bearer BQBWwLrSGv9ELtrrx4Lj1L86vjKu63Rt9cXByzeV9gFneKi4H-SXSvhGo_dIbhqReoq2JuC62mjedzoVC_I"
-    });
 
-    return this.http
-      .get(
-        `https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,
-        { headers }
-      )
-      .pipe(map(data => data["artists"].items));
+  getNewRelease() {
+    return this.getQuery("browse/new-releases?limit=20").pipe(
+      map(data => data["albums"].items)
+    );
+  }
+
+  getArtista(termino: string) {
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`).pipe(
+      map(data => data["artists"].items)
+    );
   }
 }
