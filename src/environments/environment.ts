a7480eb0d6f8274a89006d70be0f27b79d133442
crusiris <cruzsirys@gmail.com>
2020-02-08 13:14:06 -0300

    Configurando y levantando el deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAyH4pJ0bOjxnkXXzNGiCpxR7OhoFK8XKo",
+    authDomain: "spotiapp-33566.firebaseapp.com",
+    databaseURL: "https://spotiapp-33566.firebaseio.com",
+    projectId: "spotiapp-33566",
+    storageBucket: "spotiapp-33566.appspot.com",
+    messagingSenderId: "791987000560",
+    appId: "1:791987000560:web:080ab48d80f7607cd99e35"
+  }
 };
 
 /*
